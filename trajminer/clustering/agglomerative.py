971125591ac899073e7de139a52d9620d0053be0
Lucas May Petry <lucasmaypetry@gmail.com>
2019-02-18 19:00:29 -0400

    Implemented DBSCAN. Closes #1

--- trajminer/clustering/agglomerative.py
+++ trajminer/clustering/agglomerative.py
@@ -3,5 +3,8 @@ from .base import Clustering
 
 class AgglomerativeClustering(Clustering):
 
-    def __init__(self):
+    def __init__(self, measure=None, n_jobs=1):
+        pass
+
+    def fit_predict(self, X):
         pass
