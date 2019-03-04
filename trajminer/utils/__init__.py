5e75729f17a25314780075723e4c6bcdc8220738
Lucas May Petry <lucasmaypetry@gmail.com>
2019-03-04 16:10:42 -0400

    Added Geohash. Closes #14

--- trajminer/utils/__init__.py
+++ trajminer/utils/__init__.py
@@ -2,8 +2,10 @@
 """
 from .loader import TrajectoryLoader
 from .loader import CSVTrajectoryLoader
+from .geohash import Geohash
 from .stats import compute_dataset_stats
 
 __all__ = ['TrajectoryLoader',
            'CSVTrajectoryLoader',
+           'Geohash',
            'compute_dataset_stats']
---
+++ trajminer/utils/geohash.py
@@ -0,0 +1,46 @@
+class Geohash(object):
+    """Utility for encoding latitute and longitude coordinates using Geohash.
+
+    References
+    ----------
+    `Niemeyer, G. (2008). Geohash. <https://en.wikipedia.org/wiki/Geohash>`__
+    """
+
+    def __init__(self):
+        import numpy as np
+        self.base32 = ['0', '1', '2', '3', '4', '5', '6', '7',
+                       '8', '9', 'b', 'c', 'd', 'e', 'f', 'g',
+                       'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r',
+                       's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.binary = [np.asarray(list('{0:05b}'.format(x, 'b')), dtype=int)
+                       for x in range(0, len(self.base32))]
+        self.base32toBin = dict(zip(self.base32, self.binary))
+
+    def encode(self, lat, lon, precision=10, binary=False):
+        """Encodes a lat/lon pair with Geohash.
+
+        Parameters
+        ----------
+        lat : float
+            The latitude.
+        long : float
+            The longitude.
+        precision : int (default=10)
+            The length of the encoded location in Base32.
+        binary : bool (default=False)
+            If `True`, computes the binary codification of the Base32 geohash.
+
+        Returns
+        -------
+        geohash : str or array-like
+            The Base32 geohash or a binary array-like representation if
+            `binary=True`.
+        """
+        import numpy as np
+        import geohash2 as gh
+        hashed = gh.encode(lat, lon, precision)
+
+        if binary:
+            return np.concatenate([self.base32toBin[x] for x in hashed])
+
+        return hashed
