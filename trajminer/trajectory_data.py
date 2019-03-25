e7f617320ef33279426d565909ae538e85b21490
Lucas May Petry <lucasmaypetry@gmail.com>
2019-03-25 13:39:43 -0300

    Moved stats to TrajectoryData. Closes #29

--- trajminer/trajectory_data.py
+++ trajminer/trajectory_data.py
@@ -20,7 +20,7 @@ class TrajectoryData(object):
         self.tids = tids
         self.labels = labels
         self.data = np.array(data)
-        self.stats = None
+        self._stats = None
         self.tidToIdx = dict(zip(tids, np.r_[0:len(tids)]))
 
         if self.labels:
@@ -66,7 +66,7 @@ class TrajectoryData(object):
             An array of length `n_trajectories` if `unique=False`, and of
             length `n_labels` otherwise.
         """
-        if unique:
+        if unique and self.labels is not None:
             return sorted(list(set(self.labels)))
 
         return self.labels
@@ -107,3 +107,98 @@ class TrajectoryData(object):
 
         idxs = self.labelToIdx[label]
         return self.data[idxs]
+
+    def stats(self, print_stats=False):
+        """Computes statistics for the dataset.
+
+        Parameters
+        ----------
+        print_stats : bool (default=False)
+            If `True`, stats are printed.
+
+        Returns
+        -------
+        stats : dict
+            A dictionary containing the dataset statistics.
+        """
+        if self._stats:
+            if print_stats:
+                self._print_stats()
+            return self._stats
+
+        import numpy as np
+
+        traj_lengths = [len(x) for x in self.data]
+        points = np.concatenate(self.data)
+        count_not_none = lambda arr: np.sum([1 if x is not None else 0
+                                             for x in arr])
+        attr_count = [count_not_none(p) for p in points]
+
+        self._stats = {
+            'attribute': {
+                'count': len(self.attributes),
+                'min': np.min(attr_count),
+                'avg': np.mean(attr_count),
+                'std': np.std(attr_count),
+                'max': np.max(attr_count)
+            },
+            'point': {
+                'count': np.sum(traj_lengths)
+            },
+            'trajectory': {
+                'count': len(self.data),
+                'length': {
+                    'min': np.min(traj_lengths),
+                    'avg': np.mean(traj_lengths),
+                    'std': np.std(traj_lengths),
+                    'max': np.max(traj_lengths)
+                }
+            }
+        }
+
+        if self.labels:
+            unique, counts = np.unique(self.labels, return_counts=True)
+            self._stats['label'] = {
+                'count': len(unique),
+                'min': np.min(counts),
+                'avg': np.mean(counts),
+                'std': np.std(counts),
+                'max': np.max(counts)
+            }
+
+        if print_stats:
+            self._print_stats()
+        return self._stats
+
+    def _print_stats(self):
+        print('==========================================================')
+        print('                           STATS                          ')
+        print('==========================================================')
+        print('ATTRIBUTE')
+        print('  Count:           ', self._stats['attribute']['count'])
+        print('  Min:             ', self._stats['attribute']['min'])
+        print('  Max:             ', self._stats['attribute']['max'])
+        print('  Avg ± Std:        %.4f ± %.4f' % (self._stats['attribute']['avg'],
+              self._stats['attribute']['std']))
+
+        print('\nPOINT')
+        print('  Count:           ', self._stats['point']['count'])
+
+        print('\nTRAJECTORY')
+        print('  Count:           ', self._stats['trajectory']['count'])
+        print('  Min length:      ', self._stats['trajectory']['length']['min'])
+        print('  Max lenght:      ', self._stats['trajectory']['length']['max'])
+        print('  Avg length ± Std: %.4f ± %.4f' %
+              (self._stats['trajectory']['length']['avg'],
+               self._stats['trajectory']['length']['std']))
+
+        if self.labels:
+            print('\nLABEL')
+            print('  Count:           ', self._stats['label']['count'])
+            print('  Min:             ', self._stats['label']['min'])
+            print('  Max:             ', self._stats['label']['max'])
+            print('  Avg ± Std:        %.4f ± %.4f' % (self._stats['label']['avg'],
+                  self._stats['label']['std']))
+            print('==========================================================')
+        else:
+            print('==========================================================')
--- trajminer/utils/__init__.py
+++ trajminer/utils/__init__.py
@@ -3,9 +3,7 @@
 from .loader import TrajectoryLoader
 from .loader import CSVTrajectoryLoader
 from .geohash import Geohash
-from .stats import compute_dataset_stats
 
 __all__ = ['TrajectoryLoader',
            'CSVTrajectoryLoader',
-           'Geohash',
-           'compute_dataset_stats']
+           'Geohash']
