82030818a4ed6250656cdf66e47bf974bc132f84
Lucas May Petry <lucasmaypetry@gmail.com>
2019-03-28 22:52:40 -0300

    Added parallel jobs to CSV loader. Added lat/lon treatment. Closes #32

--- trajminer/utils/loader.py
+++ trajminer/utils/loader.py
@@ -1,4 +1,7 @@
+from joblib import Parallel, delayed
+from sklearn.utils import gen_even_slices
 import pandas as pd
+import numpy as np
 
 from ..trajectory_data import TrajectoryData
 
@@ -32,8 +35,20 @@ class CSVTrajectoryLoader(TrajectoryLoader):
     label_col : str (default='label')
         The column in the CSV file corresponding to the trajectory labels. If
         `None`, labels are not loaded.
+    lat : str (default='lat')
+        The column in the CSV file corresponding to the latitude of the
+        trajectory points. If both the `lat` and `lon` columns are present in
+        the file, they are included as a single new attribute in the loaded
+        dataset.
+    lon : str (default='lon')
+        The column in the CSV file corresponding to the longitude of the
+        trajectory points. If both the `lat` and `lon` columns are present in
+        the file, they are included as a single new attribute in the loaded
+        dataset.
     drop_col : array-like (default=[])
         List of columns to drop when reading the data from the file.
+    n_jobs : int (default=1)
+        The number of parallel jobs.
 
     Examples
     --------
@@ -45,38 +60,69 @@ class CSVTrajectoryLoader(TrajectoryLoader):
     """
 
     def __init__(self, file, sep=',', tid_col='tid', label_col='label',
-                 drop_col=[]):
+                 lat='lat', lon='lon', drop_col=[], n_jobs=1):
         self.file = file
         self.sep = sep
         self.tid_col = tid_col
         self.label_col = label_col
+        self.lat = lat
+        self.lon = lon
         self.drop_col = drop_col
+        self.n_jobs = n_jobs
 
     def load(self):
         df = pd.read_csv(self.file, sep=self.sep)
         attributes = list(df.keys())
-
         attributes.remove(self.tid_col)
 
         if self.label_col:
             attributes.remove(self.label_col)
 
+        lat_lon = self.lat in attributes and self.lon in attributes
+
+        if lat_lon:
+            attributes.remove(self.lat)
+            attributes.remove(self.lon)
+
         for col in self.drop_col:
             if col in attributes:
                 attributes.remove(col)
 
-        tids = df[self.tid_col].unique()
-        data = []
+        tids = sorted(df[self.tid_col].unique())
+
+        def load_tids(slice):
+            ret = []
+
+            for idx in range(slice.start, slice.stop):
+                tid = tids[idx]
+                traj = df.loc[df['tid'] == tid, attributes].values
+
+                if lat_lon:
+                    loc = df.loc[df['tid'] == tid, [self.lat, self.lon]].values
+                    new_traj = [np.append(traj[i], loc[i])
+                                for i in range(0, len(loc))]
+                    traj = new_traj
+                ret.append(traj)
+            return ret
+
         labels = None
+        func = delayed(load_tids)
+
+        data = Parallel(n_jobs=self.n_jobs, verbose=0)(
+            func(s) for s in gen_even_slices(len(tids), self.n_jobs))
+
+        data = np.concatenate(data)
 
         if self.label_col:
-            labels = []
-            for tid in tids:
-                data.append(df.loc[df['tid'] == tid, attributes].values)
-                labels.append(df.loc[df['tid'] == tid, ['label']].values[0][0])
-        else:
-            for tid in tids:
-                data.append(df.loc[df['tid'] == tid, attributes].values)
+            labels = df \
+                .drop_duplicates(subset=[self.tid_col, self.label_col],
+                                 inplace=False) \
+                .sort_values(self.tid_col,
+                             ascending=True,
+                             inplace=False)[self.label_col].values
+
+        if lat_lon:
+            attributes.append('lat_lon')
 
         return TrajectoryData(attributes=attributes,
                               data=data,
