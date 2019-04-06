d8e1f67249f2aed0c95bd9c0dff606b7310c9347
Lucas May Petry <lucasmaypetry@gmail.com>
2019-04-06 13:57:57 -0300

    Changed datasets to smaller tar.xz files refs #26

--- trajminer/datasets/base.py
+++ trajminer/datasets/base.py
@@ -1,6 +1,6 @@
 from os import path
 import tempfile
-import zipfile
+import tarfile
 
 from ..utils.loader import CSVTrajectoryLoader
 
@@ -29,21 +29,22 @@ def load_starkey_animals(n_jobs=1):
     `https://www.fs.fed.us/pnw/starkey/mapsdata.shtml
     <https://www.fs.fed.us/pnw/starkey/mapsdata.shtml>`__
     """
-    zip_file = path.join(_get_data_dir(), 'starkey_telemetry',
-                         'starkey_animals.zip')
-    csv_file = _extract_zip(zip_file)
+    tar_file = path.join(_get_data_dir(), 'starkey_telemetry',
+                         'starkey_animals.tar.xz')
+    csv_file = _extract_tar(tar_file)
     loader = CSVTrajectoryLoader(file=csv_file, sep=',', tid_col='tid',
                                  label_col='species', lat='lat', lon='lon',
                                  n_jobs=n_jobs)
     return loader.load()
 
 
-def _extract_zip(file):
+def _extract_tar(file):
     tmp = tempfile.mkdtemp()
-    zip_file = zipfile.ZipFile(file, 'r')
-    zip_file.extractall(tmp)
-    zip_file.close()
-    return path.join(tmp, zip_file.namelist()[0])
+    tar_file = tarfile.open(file, 'r')
+    tar_file.extractall(tmp)
+    extracted = path.join(tmp, tar_file.getnames()[0])
+    tar_file.close()
+    return extracted
 
 
 def _get_data_dir():
